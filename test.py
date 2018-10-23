55ae4df7d0842ea042073bbb8c33f80f711e12cd
vaderrama <juan.alvarezz.11@gmail.com>
2018-10-23 13:03:46 +0200

    Update test nd requirements close #8

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-# content of test_sample.py
-def inc(x):
-    return x + 1
+import pytest
+from main import get_weather
 
 
-def test_answer():
-    assert inc(3) == 5
+def test_get_weather():
+    weather = get_weather("Almeria, ES")
+    if type(weather) == type(dict):
+        pass
