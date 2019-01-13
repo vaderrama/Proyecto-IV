6fb07c4fb18626dbc1699a6e24b59ea67bbf7eeb
vaderrama <juan.alvarezz.11@gmail.com>
2019-01-13 12:58:53 +0100

    Actualizacion fabfile close #42

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -4,20 +4,20 @@ from fabric.api import *
 
 
 
-# Metodo para Iniciar el servicio web mediante gunicorn
+# INICIAMOS SISTEMA WEB MEDIANTE GUNICORN
 def Iniciar():
     
     # Iniciamos el servicio web mediante "gunicorn" en el puerto elegido 80
     run('cd Proyecto-IV/app/ && sudo gunicorn main:app -b 0.0.0.0:80')
 
-# Metodo para borrar antiguo codigo.
+# BORRAMOS ANTIGUO CODIGO
 def Borrar():
 
     # Borramos antiguo codigo
     run('rm -rf Proyecto-IV')
 
 
-#Metodo que borra y vuelve a descargar el repositorio de github
+# BORRA Y VUELVE A DESCARGAR EL NUEVO CODIGO 
 def Actualizar():
 
     # Borramos antiguo codigo
